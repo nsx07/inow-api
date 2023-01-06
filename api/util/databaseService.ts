7eb7e853e90295a741684846d4ceab296fbb7620
Felipe Pires <felipe.pires@weipa.com.br>
2023-01-06 14:28:21 -0300

    ssh-key

--- api/util/databaseService.ts
+++ api/util/databaseService.ts
@@ -1,10 +1,16 @@
 import { Client, ExecuteResult } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 
-const client = await new Client().connect({
-  hostname: "127.0.0.1",
-  username: "root",
-  poolSize : 5,
-  db: "inow",
-});
+
+const client = await new Client()
+  client
+    .connect({
+  
+      hostname: "144.22.234.67",
+      port: 22,
+      username: "inow-admin",
+      password : "inow@Adm_2023",
+      poolSize : 5,
+      db: "inow",
+    });
 
 export { client }
