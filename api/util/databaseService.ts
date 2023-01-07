895acc5eb4a08a67df3f5cd6b8ae32e39c3e8b25
felipe <felipeaugustodluz@hotmail.com>
2023-01-07 12:57:07 -0300

    criado utilDatabaseAction, para automatizar atividades do banco de dados

--- api/util/databaseService.ts
+++ api/util/databaseService.ts
@@ -13,4 +13,28 @@ const client = await new Client()
       db: "inow",
     });
 
+const utilDataBaseAction = {
+  initDb : async () => {
+    await client.execute(
+      `
+      CREATE TABLE USER (
+        ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        NAME VARCHAR(30) NOT NULL,
+        LASTNAME VARCHAR(75) NOT NULL,
+        CPF CHAR(11) NOT NULL UNIQUE,
+        EMAIL VARCHAR(35) NOT NULL,
+        PHONE VARCHAR(15) NOT NULL
+      );
+      CREATE TABLE SECURITY (
+        ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        U_ID INT NOT NULL,
+        SECRET VARCHAR(18) NOT NULL,
+        SECRET_KEY INT NOT NULL,
+
+        FOREIGN KEY (U_ID) REFERENCES USER(ID)
+     )`
+      )
+  }
+}
+
 export { client }
